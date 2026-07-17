3701fa12a8e8238c4e35737dbdfef4a255b90cd9
Joy_Deb_Nath <joy.cucse@gmail.com>
2026-07-18 03:51:51 +0600

    Add seed command to build script

--- build.sh
+++ build.sh
@@ -8,5 +8,8 @@ pip install -r requirements.txt
 # Run migrations
 python manage.py migrate
 
+# Seed database (TEMPORARY: Runs seed_ems to create test users)
+python manage.py seed_ems
+
 # Collect static files
 python manage.py collectstatic --no-input
