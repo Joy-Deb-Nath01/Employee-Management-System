5c1993bc82c59a7b3d8e06fe7af706d73eed196a
Joy_Deb_Nath <joy.cucse@gmail.com>
2026-07-18 04:07:28 +0600

    Add seed command back temporarily for database migration

--- build.sh
+++ build.sh
@@ -8,5 +8,8 @@ pip install -r requirements.txt
 # Run migrations
 python manage.py migrate
 
+# Seed database (Temporary: Will run once when database URL is connected)
+python manage.py seed_ems
+
 # Collect static files
 python manage.py collectstatic --no-input
